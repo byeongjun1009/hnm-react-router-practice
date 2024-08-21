c79255ea138d91110dc8df81bafae89549137120
DESKTOP-F45SLBC\bnj30 <byeongjunkim1009@gmail.com>
2024-08-22 01:57:56 +0900

    commit

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src="https://noona-hnm.netlify.app/pattern-jacket.jpeg" alt="" />
+        <div>벨티드 트윌 코트</div>
+        <div>99900</div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default ProductCard
