12ad7d579482736f063be8e6453ac71b23481e4d
DESKTOP-F45SLBC\bnj30 <byeongjunkim1009@gmail.com>
2024-08-25 22:57:40 +0900

    authenticate 수정

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import PrivateRoute from './route/PrivateRoute';
 // onSubmit: submit에 대해서는 onSubmit이라는 이벤트 줘야 함.
 function App() {
 
-  const [authenticate, setAuthenticate] = useState(true)
+  const [authenticate, setAuthenticate] = useState(false)
 
   useEffect(() => {
 
